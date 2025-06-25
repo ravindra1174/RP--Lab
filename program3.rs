05826f67de75a9886a741a22610a65945b6a602d
ravindra1174 <22pa1a05c5@vishnu.edu.in>
2025-06-25 10:35:47 +0530

    Add files via upload

---
+++ program3.rs
@@ -0,0 +1,9 @@
+fn main(){
+    let x = 1000;
+    let y = "Programming";
+    println!("x value is {}",x);
+    println!("y value is {}",y);
+    let x = 1100;
+    println!("x value is {}",x);
+    println!("y value is {}",y);
+}
