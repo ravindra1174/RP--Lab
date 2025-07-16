5c3051163c0c62d1f7e4a85eea19e62639f675b2
ravindra1174 <22pa1a05c5@vishnu.edu.in>
2025-07-16 11:52:30 +0530

    Create pos_neg.rs

---
+++ pos_neg.rs
@@ -0,0 +1,12 @@
+fn main(){
+    let num = 7;
+    if num>0{
+        println!("positive");
+    }
+    else if num==0{
+        println!("zero");
+    }
+    else{
+        println!("Negative");
+    }
+}
